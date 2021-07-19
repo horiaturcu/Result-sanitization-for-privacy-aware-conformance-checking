38c1a83720089cc77292d63115e2ac845f52814d
horiaturcu <87600202+horiaturcu@users.noreply.github.com>
2021-07-19 18:05:01 +0200

    Update Conformance_sanitise_Main_program.py

--- Conformance_sanitise_Main_program.py
+++ Conformance_sanitise_Main_program.py
@@ -2,6 +2,7 @@
 
 
 
+#The following code requires pm4py this can be done by running the console and writing "pip install pm4py" it also uses the Mini-conda python.
 class k_anon :
     import os
     from pm4py.objects.log.importer.xes import importer as xes_importer
@@ -12,6 +13,7 @@ class k_anon :
     from difflib import SequenceMatcher
 
     print('i am starting the import')
+    # The "D:\UniMannheim\Bachelor\Event logs\A.xes" in the next line can be changed in order to show the path to the xes or csv file you want the inductive miner to run on
     log = xes_importer.apply(os.path.join("tests", "input_data", r"D:\UniMannheim\Bachelor\Event logs\A.xes"))
     print('i finished the import')
     net, initial_marking, final_marking = inductive_miner.apply(log,variant = inductive_miner.Variants.IMf,parameters={inductive_miner.Variants.IMf.value.Parameters.NOISE_THRESHOLD : 0.3})
@@ -21,6 +23,8 @@ class k_anon :
     def list_in(a, b):
         return any(map(lambda x: b[x:x + len(a)] == a, range(len(b) - len(a) + 1)))
 
+
+    #The following for statement can be commented out in order to keep the (>>,None) alignments in the log
     for x in range(len(aligned_traces)) :
         for y in reversed(range(len(aligned_traces[x]['alignment']))) :
             if aligned_traces[x]['alignment'][y][0] == '>>' and aligned_traces[x]['alignment'][y][1] == None :
@@ -53,13 +57,12 @@ class k_anon :
         for y in range(spot,len(aligned_traces)) :
             flag = 0
             already_here = x
-
-            #string1 = str(aligned_list[x]['alignment'])
             string2 = str(aligned_list[y]['alignment'])
             match = SequenceMatcher(None, string1,string2).find_longest_match(0,len(string1),0,len(string2))
             size = len(string1[match.a: match.a +match.size])
             mod_string = string1[match.a: match.a +match.size]
-            if len(mod_string) < 20 :
+            #This if statement can also be increased or decreased depending on how long/short you want your sub-sequences, default 40 in order to display a minimum of 3 tuples.
+            if len(mod_string) < 40 :
                 continue
             
 
@@ -72,9 +75,6 @@ class k_anon :
                 continue
             print(mod_string)
             sequence = eval('[' + mod_string +']')
-            # if len(sequence) > maxseq :
-            #     maxseq = len(sequence)
-            #     primeseq = sequence
             if counter == 0 :
                 lcs_dict = {'alignment' : sequence, 'occurence' : 1, 'confirmed' : -1}
                 lcs_list.append(lcs_dict)
@@ -92,7 +92,7 @@ class k_anon :
                     lcs_list.append(lcs_dict)
     
                 
-
+    # the if statement can be increased or decreased, default 5 in order to achieve higher or lower k-anonimity
     k_list = list()
     for x in lcs_list :
         if x['occurence'] >= 5 :
@@ -101,8 +101,10 @@ class k_anon :
     import pandas as pd
     from pm4py.objects.conversion.log import converter as log_converter
     dataframe = log_converter.apply(k_list, variant=log_converter.Variants.TO_DATA_FRAME)
-    dataframe.to_csv(r"D:\UniMannheim\Bachelor\Event logs\results-expAtest.csv")
+    # The "D:\UniMannheim\Bachelor\Event logs\results-expAtest.csv" can be chenged in order to export the final log to another location Note that this exports a csv file always
+    dataframe.to_csv(r"D:\UniMannheim\Bachelor\Event logs\results-expCtest.csv")
 
+    #The following for statement exists to show all the skips present in the end-event log, it prints them to the console, this for statement can be commented out if they do not present an interest
     cald = list()
     for x in range(len(k_list)) :
         for y in range(len(k_list[x]['alignment'])) :
